ba35e4568a15454d3bca8580363233912ee8718c
Emre Şahiner <emre@workintech.com.tr>
2023-10-10 11:29:53 +0300

    users added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,60 @@ import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import Footer from "./layouts/Footer";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import axios from "axios";
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState({
-    name: "Emre",
-    password: "1234",
-  });
+  const [loggedInUser, setLoggedInUser] = useState({});
   const [allUsers, setAllUsers] = useState([]);
   const [tasks, setTasks] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!loggedInUser.name) {
+      history.push("/login");
+    }
+  }, []); //DidMount
+
+  useEffect(() => {
+    axios
+      .get("https://reqres.in/api/users?per_page=20")
+      .then((res) => {
+        setAllUsers(res.data.data);
+      })
+      .catch((err) => {
+        console.error(err.response);
+      });
+  }, []);
 
   const handleLogin = (credentials) => {
     //login credentials check in allUsers
     //setLoggedInUser
+
+    const user = allUsers.find(
+      (user) =>
+        user.first_name === credentials.password &&
+        user.email === credentials.email
+    );
+
+    if (user) {
+      setLoggedInUser(user);
+      history.push("/tasks");
+    } else {
+      console.error("User login", credentials);
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedInUser({});
+    history.push("/login");
   };
 
   //template
   return (
     <div className="App">
-      <Header user={loggedInUser} />
+      <Header user={loggedInUser} handleLogout={handleLogout} />
       <Main handleLogin={handleLogin} tasks={tasks} allUsers={allUsers} />
       <Footer />
     </div>
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,5 +1,19 @@
-const UserList = () => {
-  return <div className="userlist-container">UserList Area</div>;
+import User from "../components/User";
+import "./UserList.css";
+
+const UserList = (props) => {
+  const { allUsers } = props;
+
+  return (
+    <>
+      <h2>USERS</h2>
+      <div className="users-container">
+        {allUsers.map((user) => (
+          <User user={user} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default UserList;
